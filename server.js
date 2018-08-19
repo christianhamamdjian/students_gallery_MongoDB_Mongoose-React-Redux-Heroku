9a61c32d2133e4c0a4aa8a1a8e5706bac588f3c3
Christian Hamamdjian <christian.hamamdjian@welho.com>
2018-08-19 13:54:12 +0300

    Full stack app

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const Student = mongoose.model("Student", {
   photo: { data: Buffer, contentType: String },
   src: {
