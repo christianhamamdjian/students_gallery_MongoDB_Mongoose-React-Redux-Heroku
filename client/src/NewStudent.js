422a96da46a36f0419a36c3a5b41b30c660d9d14
Christian Hamamdjian <christian.hamamdjian@welho.com>
2018-11-04 17:17:46 +0200

    Added Redux Form

--- client/src/NewStudent.js
+++ client/src/NewStudent.js
@@ -2,59 +2,45 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "./App.css";
 import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+
+const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length < 4) {
+    errors.firstName = "Must be 4 characters or more";
+  }
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length < 4) {
+    errors.lastName = "Must be 4 characters or more";
+  }
+  return errors;
+};
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        ((error && <span className="message">{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+);
 
 class NewStudent extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      photo: "",
-      src: "",
-      alt: "",
-      firstName: "",
-      lastName: "",
-      title: "",
-      nationality: "",
-      skills: "",
-      whySofterDeveloper: "",
-      motivatesMe: "",
-      longTermVision: "",
-      favoriteQuote: "",
-      joinedOn: ""
-    };
-  }
 
   handleSave = e => {
     e.preventDefault();
-    const photo = this.state.photo;
-    const src = this.state.photo.name;
-    const alt = this.state.photo.name;
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
-    const title = this.state.title;
-    const nationality = this.state.nationality;
-    const skills = this.state.skills;
-    const whySofterDeveloper = this.state.whySofterDeveloper;
-    const longTermVision = this.state.longTermVision;
-    const motivatesMe = this.state.motivatesMe;
-    const favoriteQuote = this.state.favoriteQuote;
-    const joinedOn = this.state.joinedOn;
-
-    const formData = new FormData();
-    formData.append("photo", photo);
-    formData.append("src", src);
-    formData.append("alt", alt);
-    formData.append("firstName", firstName);
-    formData.append("lastName", lastName);
-    formData.append("title", title);
-    formData.append("nationality", nationality);
-    formData.append("skills", skills);
-    formData.append("whySofterDeveloper", whySofterDeveloper);
-    formData.append("longTermVision", longTermVision);
-    formData.append("motivatesMe", motivatesMe);
-    formData.append("favoriteQuote", favoriteQuote);
-    formData.append("joinedOn", joinedOn);
-
+    let payload = this.props.posts.form.newPost.values;
     const request = async () => {
       try {
         const res = await fetch("/api/newstudent", {
@@ -65,7 +51,7 @@ class NewStudent extends Component {
           body: formData
         });
         const json = await res.json().then(response => {
-          this.props.handleSubmit(response);
+          this.props.onSubmit(response);
           this.props.history.push("/");
         });
         return json;
@@ -74,185 +60,157 @@ class NewStudent extends Component {
       }
     };
     request();
+
   };
 
-  addPhoto(addPhoto) {
-    this.setState({ photo: addPhoto.target.files[0] });
-  }
-  addSrc(addSrc) {
-    this.setState({ src: addSrc });
-  }
-  addAlt(addAlt) {
-    this.setState({ alt: addAlt });
-  }
-  addFirstName(addFirstName) {
-    this.setState({ firstName: addFirstName });
-  }
-  addLastName(addLastName) {
-    this.setState({ lastName: addLastName });
-  }
-  addTitle(addTitle) {
-    this.setState({
-      title: addTitle
-    });
-  }
-  addNationality(addNationality) {
-    this.setState({ nationality: addNationality });
-  }
-  addSkills(addSkills) {
-    this.setState({ skills: addSkills });
-  }
-  addWhySofterDeveloper(addWhySofterDeveloper) {
-    this.setState({ whySofterDeveloper: addWhySofterDeveloper });
-  }
-  addMotivatesMe(addMotivatesMe) {
-    this.setState({ motivatesMe: addMotivatesMe });
-  }
-  addLongTermVision(addLongTermVision) {
-    this.setState({ longTermVision: addLongTermVision });
-  }
-  addFavoriteQuote(addFavoriteQuote) {
-    this.setState({ favoriteQuote: addFavoriteQuote });
-  }
-  addJoinedOn(addJoinedOn) {
-    this.setState({ joinedOn: addJoinedOn });
-  }
   render() {
     return (
       <div className="container">
         <NavLink to="/">
           <i className="fas fa-2x fa-angle-double-left" />
         </NavLink>
-        <form method="post">
-          <label htmlFor="firstName">Upload photo:</label>
-          <input
-            className="photo"
-            id="photo"
-            name="photo"
-            type="file"
-            multiple="multiple"
-            onChange={e => this.addPhoto(e)}
+
+        <form method="post" onSubmit={this.handleSave}>
+          <Field
+            placeholder="Photo"
+            name="addPhoto"
+            type="text"
+            component={renderField}
+            label="First Name"
           />
 
-          <label htmlFor="firstName">First name:</label>
-          <input
-            id="firstName"
+          <Field
+            placeholder="Src"
+            name="addSrc"
+            type="text"
+            component={renderField}
+            label="First Name"
+          />
+
+          <Field
+            placeholder="Alt"
+            name="addAlt"
+            type="text"
+            component={renderField}
+            label="First Name"
+          />
+
+          <Field
+            placeholder="First Name"
             name="firstName"
             type="text"
-            placeholder="First name"
-            onChange={e => this.addFirstName(e.target.value)}
+            component={renderField}
+            label="First Name"
           />
 
-          <label htmlFor="lastName">Last name:</label>
-          <input
-            id="lastName"
+          <Field
+            placeholder="Last Name"
             name="lastName"
             type="text"
-            placeholder="Last name"
-            onChange={e => this.addLastName(e.target.value)}
+            component={renderField}
+            label="Last Name"
           />
 
-          <label htmlFor="title">Title:</label>
-          <input
-            id="title"
-            name="title"
-            type="text"
+          <Field
             placeholder="Title"
-            onChange={e => this.addTitle(e.target.value)}
+            name="addTitle"
+            type="text"
+            component={renderField}
+            label="Title"
           />
 
-          <label htmlFor="nationality">Nationality:</label>
-          <input
-            id="nationality"
-            name="nationality"
-            type="text"
+          <Field
             placeholder="Nationality"
-            onChange={e => this.addNationality(e.target.value)}
+            name="addNationality"
+            type="text"
+            component={renderField}
+            label="Nationality"
           />
 
-          <label htmlFor="skills">Skills:</label>
-          <input
-            id="skills"
-            name="skills"
-            type="text"
+          <Field
             placeholder="Skills"
-            onChange={e => this.addSkills(e.target.value)}
+            name="addSkills"
+            type="text"
+            component={renderField}
+            label="TSkillsemp"
           />
 
-          <label htmlFor="whySofterDeveloper">Why a software developer:</label>
-          <input
-            id="whySofterDeveloper"
-            name="whySofterDeveloper"
+          <Field
+            placeholder="Why a Software Developer"
+            name="addWhySofterDeveloper"
             type="text"
-            placeholder="Why a software developer"
-            onChange={e => this.addWhySofterDeveloper(e.target.value)}
+            component={renderField}
+            label="Why a Software Developer"
           />
 
-          <label htmlFor="longTermVision">Long term vision:</label>
-          <input
-            id="longTermVision"
-            name="longTermVision"
+          <Field
+            placeholder="Motivates Me"
+            name="addMotivatesMe"
             type="text"
-            placeholder="Long term vision"
-            onChange={e => this.addLongTermVision(e.target.value)}
+            component={renderField}
+            label="Motivates Me"
           />
 
-          <label htmlFor="motivatesMe">What motivates me:</label>
-          <input
-            id="motivatesMe"
-            name="motivatesMe"
+          <Field
+            placeholder="Long Term Vision"
+            name="addLongTermVision"
             type="text"
-            placeholder="What motivates me"
-            onChange={e => this.addMotivatesMe(e.target.value)}
+            component={renderField}
+            label="Long Term Vision"
           />
 
-          <label htmlFor="favoriteQuote">Favorite quote:</label>
-          <input
-            id="favoriteQuote"
-            name="favoriteQuote"
+          <Field
+            placeholder="Favorite Quote"
+            name="addFavoriteQuote"
             type="text"
-            placeholder="Favorite quote"
-            onChange={e => this.addFavoriteQuote(e.target.value)}
+            component={renderField}
+            label="Favorite Quote"
           />
 
-          <label htmlFor="joinedOn">Joined on:</label>
-          <input
-            id="joinedOn"
-            name="joinedOn"
+          <Field
+            placeholder="Joined On"
+            name="addJoinedOn"
             type="text"
-            placeholder="Joined on"
-            onChange={e => this.addJoinedOn(e.target.value)}
+            component={renderField}
+            label="Joined On"
           />
+
 
           <button
             className="button-save"
             type="submit"
-            onClick={this.handleSave}
           >
             <i className="fa fa-3x fa-check-circle" />
           </button>
           <NavLink className="button-cancel" to="/">
             <i className="fa fa-2x fa-window-close" />
           </NavLink>
-        </form>
       </div>
     );
-  }
-}
-
+      }
+    }
+    
 const mapStateToProps = state => {
   return {
-    students: state
-  };
-};
-
+          students: state
+      };
+    };
+    
 const mapDispatchToProps = dispatch => {
   return {
-    handleSubmit: payload => dispatch({ type: "ADD_STUDENT", payload })
-  };
-};
+          onSubmit: payload => dispatch({type: "ADD_STUDENT", payload })
+      };
+    };
+    
+const myForm = reduxForm({
+          form: "newStudent",
+        validate
+        // warn
+      })(NewStudent);
+      
+      export default connect(
+        mapStateToProps,
+        mapDispatchToProps
+      )(myForm);
+      
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NewStudent);
